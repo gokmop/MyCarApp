e607c9722e71fec4248fe633839febe90b2aea41
Georgi Shutov <g.h.shutov@gmail.com>
2022-12-03 23:25:36 +0200

    Brand and BrandRepository fixed;

--- my-car-app/src/main/java/com/mca/mycarapp/repository/BrandRepository.java
+++ my-car-app/src/main/java/com/mca/mycarapp/repository/BrandRepository.java
@@ -3,6 +3,7 @@ package com.mca.mycarapp.repository;
 import com.mca.mycarapp.models.Brand;
 import com.mca.mycarapp.repository.base.MyCarRepository;
 
+
 import java.util.UUID;
 
 public interface BrandRepository extends MyCarRepository<Brand, UUID> {
