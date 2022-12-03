922ed6a768b9d67ce0cedb2a4bb78b32f9cfe419
Georgi Shutov <g.h.shutov@gmail.com>
2022-12-03 16:17:39 +0200

    BrandServiceImpl fixed.

--- my-car-app/src/main/java/com/mca/mycarapp/services/impl/BrandServiceImpl.java
+++ my-car-app/src/main/java/com/mca/mycarapp/services/impl/BrandServiceImpl.java
@@ -1,8 +1,33 @@
 package com.mca.mycarapp.services.impl;
 
+import com.mca.mycarapp.models.Brand;
+import com.mca.mycarapp.repository.BrandRepository;
 import com.mca.mycarapp.services.contracts.BrandService;
+import com.mca.mycarapp.services.contracts.base.GetService;
+import com.mca.mycarapp.services.contracts.base.GetServiceBase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
-public class BrandServiceImpl{
+public class BrandServiceImpl extends GetServiceBase implements BrandService, GetService {
+
+    private final BrandRepository brandRepository;
+
+    @Autowired
+    public BrandServiceImpl(BrandRepository brandRepository) {
+        this.brandRepository = brandRepository;
+    }
+
+    @Override
+    public List<Brand> getAll() {
+        return brandRepository.findAll();
+    }
+
+    @Override
+    public Brand getOne(UUID uuid) {
+        return get(brandRepository, uuid, Brand.class);
+    }
 }
