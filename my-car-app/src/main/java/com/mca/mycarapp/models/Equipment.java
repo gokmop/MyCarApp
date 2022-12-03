7accd5730c268b2c30f4ef4cd9cee49cd6cc0c07
Georgi Shutov <g.h.shutov@gmail.com>
2022-12-03 23:26:14 +0200

    Equipment Entity and DB table linked;

--- my-car-app/src/main/java/com/mca/mycarapp/models/Equipment.java
+++ my-car-app/src/main/java/com/mca/mycarapp/models/Equipment.java
@@ -1,10 +1,21 @@
 package com.mca.mycarapp.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import java.util.UUID;
 
+@Entity
+@Table(name = "equipment")
 public class Equipment {
-    private UUID id;
+    @Id
+    @Column(name = "id")
+    private UUID id = UUID.randomUUID();
+    @Column(name = "name")
     private String name;
+    @Column(name = "description")
     private String description;
 
     public Equipment() {
